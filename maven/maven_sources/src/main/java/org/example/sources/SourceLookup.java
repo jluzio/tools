21c521304b2cd8db66d4ef8b0771fcaeb31dfb44
João Luzio <joao.luzio@gmail.com>
2021-07-31 00:04:44 +0100

    import

---
+++ maven/maven_sources/src/main/java/org/example/sources/SourceLookup.java
@@ -0,0 +1,11 @@
+package org.example.sources;
+
+import java.time.LocalDate;
+
+public class SourceLookup {
+
+  public static void main(String[] args) throws Exception {
+    System.out.println(LocalDate.now().plusDays(30 + 1));
+  }
+
+}
